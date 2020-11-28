2798c43a7b3f5d313d4235ff96d4736360b9a96e
harrysong99 <hssong319@gmail.com>
2020-11-28 01:31:41 -0800

    profile layout, edit feature

--- client/src/components/dashboard/Edit.js
+++ client/src/components/dashboard/Edit.js
@@ -4,13 +4,15 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getUser } from "../../actions/userActions";
 
-const serviceOption = [
+import Select from "react-select";
+
+const services = [
   { label: "1부 찬양/미디어 (7:45AM)", value: "1" },
   { label: "1부 섬김이 예배 (8:00AM)", value: "2" },
   { label: "2부 찬양/미디어 (9:30AM)", value: "3" },
   { label: "2부 (10:10AM)", value: "4" },
 ];
-const locationOption = [
+const locations = [
   {
     label: "1,2: Downtown Berkeley Bart Station (2160 Shattuck Ave)",
     value: "1",
@@ -20,6 +22,10 @@ const locationOption = [
   { label: "7: Clark Kerr (2601 Warring St)", value: "4" },
   { label: "8: Foothill (2700 Hearst Ave)", value: "5" },
   { label: "Other", value: "6" },
+];
+const driverOption = [
+  { label: "Yes", value: "true" },
+  { label: "No", value: "false" },
 ];
 
 class Profile extends Component {
@@ -52,8 +58,8 @@ class Profile extends Component {
       this.setState({
         name: userInfo.name,
         email: userInfo.email,
-        location: locationOption[userInfo.location].label,
-        serviceTime: serviceOption[userInfo.serviceTime].label,
+        location: locations[userInfo.location].label,
+        serviceTime: services[userInfo.serviceTime].label,
         driver: driver,
         surveyCompleted: surveyCompleted,
       });
@@ -62,9 +68,26 @@ class Profile extends Component {
   onSubmit = (e) => {
     e.preventDefault();
   };
+  onChange = (e) => {
+    if (locations.includes(e)) {
+      this.setState({
+        location: e.value,
+      });
+    } else if (services.includes(e)) {
+      this.setState({
+        serviceTime: e.value,
+      });
+    } else if (driverOption.includes(e)) {
+      this.setState({
+        driver: e.value,
+      });
+    } else {
+      this.setState({
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
   render() {
-    const { errors } = this.state;
-    console.log(this.state);
     return (
       <div className="container">
         <div style={{ marginTop: "4rem" }} className="row">
@@ -73,6 +96,94 @@ class Profile extends Component {
               <i className="material-icons left">keyboard_backspace</i> Back to
               profile
             </Link>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>Edit Profile</h4>
+              <form noValidate onSubmit={this.onSubmit}>
+                <div id="edit-profile-form" class="dialog-box">
+                  <p>
+                    <label for="username">Full Name:</label>
+                    <input
+                      onChange={this.onChange}
+                      type="text"
+                      id="name"
+                      value={this.state.name}
+                    ></input>
+                  </p>
+                  <p>
+                    <label for="email">Email:</label>
+                    <input
+                      onChange={this.onChange}
+                      type="email"
+                      id="email"
+                      value={this.state.email}
+                    ></input>
+                  </p>
+                  <p>
+                    <p>
+                      <label for="location">Location:</label>
+                    </p>
+                    <Select
+                      required={true}
+                      onChange={this.onChange}
+                      id="location"
+                      options={locations}
+                      value={locations.find(
+                        (obj) => obj.value === this.state.serviceTime
+                      )}
+                      placeholder={this.state.location}
+                      searchable={false}
+                      menuPlacement="auto"
+                    />
+                  </p>
+                  <p>
+                    <p>
+                      <label for="serviceTime">Service Time:</label>
+                    </p>
+                    <Select
+                      required={true}
+                      onChange={this.handleSerivce}
+                      id="serviceTime"
+                      options={services}
+                      value={services.find(
+                        (obj) => obj.value === this.state.serviceTime
+                      )}
+                      placeholder={this.state.serviceTime}
+                      searchable={false}
+                      menuPlacement="top"
+                    />
+                  </p>
+                  <p>
+                    <p>
+                      <label for="driver">Driver:</label>
+                    </p>
+                    <Select
+                      required={true}
+                      onChange={this.handleSerivce}
+                      id="driver"
+                      options={driverOption}
+                      value={driverOption.find(
+                        (obj) => obj.value === this.state.serviceTime
+                      )}
+                      placeholder={this.state.driver}
+                      searchable={false}
+                      menuPlacement="top"
+                    />
+                  </p>
+                  <Link
+                    style={{
+                      width: "150px",
+                      borderRadius: "3px",
+                      letterSpacing: "1.5px",
+                      marginTop: "1rem",
+                    }}
+                    type="submit"
+                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                  >
+                    Save Changes
+                  </Link>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
