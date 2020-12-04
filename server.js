33dda238430cc1745cdfb3be32ddf9b3887e4681
harrysong99 <hssong319@gmail.com>
2020-12-03 23:36:41 -0800

    version 1.2.0

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
 const passport = require("passport");
 const users = require("./routes/userRoutes");
@@ -12,7 +14,7 @@ app.use(
 );
 app.use(bodyParser.json());
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = process.env.MONGODB_URI || require("./config/keys").mongoURI;
 // Connect to MongoDB
 mongoose
   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
@@ -25,7 +27,17 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+// Serve static assets if in production
+if (process.env.NODE_ENV == "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port
+
 app.listen(port, () =>
   console.log(`backend server up and running on port ${port}!`)
 );
