f455767cbf7cce090fce383a2c44f9802d7b5579
harrysong99 <hssong319@gmail.com>
2020-12-04 23:28:19 -0800

    ready for heroku

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,13 @@
-// import credentials
+// NOTE: uncomment the lines below for dev env
+// // import credentials
 // const credentials = require("../credentials.json");
-// retrieve secrets from credentials
+// // retrieve secrets from credentials
 // const username = credentials.username;
 // const dbname = credentials.dbname;
 // const password = credentials.password;
 // const secretOrKey = credentials.secretOrKey;
+
+// NOTE: for deployment
 const username = process.env.USERNAME;
 const dbname = process.env.DBNAME;
 const password = process.env.PASSWORD;
